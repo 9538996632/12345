7b408ac39944697c3ecaa2fbe7068d2b8c9cbd4e
9538996632 <35676656+9538996632@users.noreply.github.com>
2018-02-12 09:50:04 +0530

    Delete mail sec6.py

--- mail sec6.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login('jaisurya190@gmail.com','xxxxxx')
-
-msg="your message"
-server.sendmail("jaisurya190@gmail.com","vijayshiva258@gmail.com",msg)
-server.quit()
-
