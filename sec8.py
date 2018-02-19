336ddf9111f2f7fadf8029e3cf4d8da78346249b
9538996632 <35676656+9538996632@users.noreply.github.com>
2018-02-19 09:43:12 +0530

    Add files via upload

---
+++ sec8.py
@@ -0,0 +1,15 @@
+for i in " python ":
+	if (i=='h'):
+		break
+	print(i)
+
+	
+for i in "jaisurya":
+	if(i=='i'):
+		continue
+	print(i)
+for i in range (1,2,3(23,24):
+    print(i)
+                
+
+    
