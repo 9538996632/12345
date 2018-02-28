0b4c6911a0fe35428560e734c3a596869f2f1061
9538996632 <35676656+9538996632@users.noreply.github.com>
2018-02-28 10:14:47 +0530

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,22 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: /home/cs2017a205/prathik/algorithm1.py ==============
+Traceback (most recent call last):
+  File "/home/cs2017a205/prathik/algorithm1.py", line 18, in <module>
+    quickSort(alist)
+NameError: name 'quickSort' is not defined
+>>> 
+============== RESTART: /home/cs2017a205/prathik/algorithm1.py ==============
+[17, 20, 26, 31, 44, 54, 55, 77, 93]
+>>> 
+============== RESTART: /home/cs2017a205/prathik/algorithm1.py ==============
+[9, 12, 34, 56, 67, 87, 98]
+>>> 
+============== RESTART: /home/cs2017a205/prathik/algorithm1.py ==============
+[9, 12, 34, 56, 67, 87, 98]
+>>> 
+============== RESTART: /home/cs2017a205/prathik/algorithm1.py ==============
+[12, 34, 34, 34, 56, 56, 78, 98, 123]
+>>> 
---
+++ dictionary.py
@@ -0,0 +1,2 @@
+s={78:'red','blue':78,'black':94}
+print(s[78])
